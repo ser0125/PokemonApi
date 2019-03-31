80befd0a58b269a1093b3f255349a99997a3a462
Sergio Andres Llanos Garcia <sallanos@bancolombia.com.co>
2019-03-30 19:34:19 -0500

    Se agrega el servicio de consultar la lista de pokemones

--- MyPokedex/src/app/app.module.ts
+++ MyPokedex/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { PokemonListComponent } from './components/pokemon/pokemon-list/pokemon-list.component';
 import { P404Component } from './components/error/p404/p404.component';
 import { AppRoutingModule } from './app.routing.module';
+import { PokeApiService } from './services/pokeapi.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,10 +23,12 @@ import { AppRoutingModule } from './app.routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatCardModule,
     MatCheckboxModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PokeApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- MyPokedex/src/app/components/pokemon/pokemon-list/pokemon-list.component.ts
+++ MyPokedex/src/app/components/pokemon/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.sass']
+  styleUrls: ['./pokemon-list.component.scss']
 })
 export class PokemonListComponent implements OnInit {
+  pokemons: [];
 
-  constructor() { }
+  constructor(private pokeapiservice: PokeApiService) { }
 
   ngOnInit() {
+    this.pokeapiservice.getPokemons().subscribe((data:any) => {
+      this.pokemons = data.results;
+    })
   }
 
 }
